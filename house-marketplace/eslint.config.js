11a03d6ebf3a41a8a008e130b344fdb640e9b5a7
Iva Burić <ivaburic1@gmail.com>
2024-10-16 14:03:37 +0200

    finished login

--- house-marketplace/eslint.config.js
+++ house-marketplace/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -20,6 +21,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
--- house-marketplace/src/pages/Profile.tsx
+++ house-marketplace/src/pages/Profile.tsx
@@ -1,21 +1,52 @@
-import { getAuth, User } from "firebase/auth";
-import { useState, useEffect } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { db } from "../firebase.config";
+import { updateDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
-    name: auth.currentUser?.displayName,
-    email: auth.currentUser?.email,
+    name: auth.currentUser?.displayName || "",
+    email: auth.currentUser?.email || "",
   });
 
-  const { name, email } = formData;
+  const { name } = formData;
 
   const navigate = useNavigate();
 
   const onLogout = () => {
     auth.signOut();
     navigate("/");
+  };
+
+  const onSubmit = async () => {
+    try {
+      const currentUser = auth.currentUser;
+
+      if (currentUser && currentUser.displayName !== name) {
+        await updateProfile(currentUser, {
+          displayName: name,
+        });
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
   return (
@@ -26,6 +57,34 @@ function Profile() {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              if (changeDetails) {
+                onSubmit();
+              }
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "Done" : "Change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
